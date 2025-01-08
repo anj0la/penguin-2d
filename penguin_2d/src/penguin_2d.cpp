947c95912578ddfa38dc5568d15ddd2c06f51c4e
anj0la <anjola.aina@gmail.com>
2025-01-07 22:01:46 -0700

    Removed duplicate clear() and present() calls, removing them out of input event checks so they are called once (need to delay for framerate after delta time implementation).

--- penguin_2d/src/penguin_2d.cpp
+++ penguin_2d/src/penguin_2d.cpp
@@ -10,7 +10,6 @@ void display_text(PenguinTextRenderer& text_engine, PenguinRenderer& renderer, c
     PenguinText text(text_engine, "C:\\Users\\anjol\\source\\repos\\penguin_2d\\penguin_2d\\fonts\\pixelify_sans_regular.ttf", text_p, size);
     text.set_text_colour(Colours::WHITE);
     text.render_text(position);
-    renderer.present();
 }
 
 int main(int argc, char* argv[]) {
@@ -52,6 +51,7 @@ int main(int argc, char* argv[]) {
     PenguinTextRenderer text_engine(renderer);
 
     display_text(text_engine, renderer, "Hello Penguin 2D!", 24.0f, Vector2<>(480.0 / 2, 10));
+    renderer.present();
 
 
     // So we should be updating text by creating a text object, then destroying it once it isn't needed anymore. It is still rendered onto the screen,
@@ -63,25 +63,26 @@ int main(int argc, char* argv[]) {
 
         event_handler.poll_events();
 
+        // Clear renderer for drawing
+        renderer.clear();
+
         // Check if the game should be quit (i.e., close the window)
         if (event_handler.should_quit()) {
             game_running = false;
         }
 
         if (input.is_key_pressed(PenguinKey::KEY_A)) { // Render the rect onto the screen.
-            renderer.clear();
             renderer.draw_rect(Rect2<float>(Vector2<float>(100.0, 100.0), Vector2<float>(100.0, 100.0)), Colours::WHITE, Colours::RED);
-            //text.render_text(Vector2<float>(300.0, 300.0));
-            renderer.present();
         }
         if (input.is_key_pressed(PenguinKey::ESC)) {
             game_running = false; // Close the window.
         }
         if (input.is_key_pressed(PenguinKey::KEY_X)) {
-            renderer.clear();
             display_text(text_engine, renderer, "You pressed the X button!", 24.0f, Vector2<>(480.0 / 2, 400));
-
         }
+
+        // Present renderer after finishing drawing
+        renderer.present();
 
     }
 
